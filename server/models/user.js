4f8ba97e8a15104d115835ce65d27a90d345a223
qnrjs42 <qnrjs42@naver.com>
2020-08-27 19:45:44 +0900

    Backend - 07, MongoDB Modeling 01

---
+++ server/models/user.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import momment from "momment";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["MainJuin", "SubJuin", "User"],
+    default: "User",
+  },
+  register_date: {
+    type: Date,
+    default: momment().format("YYYY-MM-DD hh:mm:ss"),
+  },
+  comments: [
+    {
+      post_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "posts",
+      },
+      comment_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "comments",
+      },
+    },
+  ],
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "posts",
+    },
+  ],
+});
+
+const User = mongoose.model("user", UserSchema);
+
+export default User;
