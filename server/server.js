6c64afe5f05ebc8c0787fb60f4171200f79a275d
qnrjs42 <qnrjs42@naver.com>
2020-08-28 15:49:47 +0900

    Backend - 12, Auth Middleware // 포트번호 .env로 이동

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
-import app from './app';
+import app from "./app";
 
-app.listen('7000', ()=> {
-    console.log('hi');
-})
+import config from "./config/index";
+const { PORT } = config;
+
+app.listen(PORT, () => {
+  console.log(`Server stated on Port ${PORT}`);
+});
