2908fadef588fb19c4ae0a29d7b181c069f4e743
qnrjs42 <qnrjs42@naver.com>
2020-08-29 17:14:51 +0900

    Frontend - 17, Router Basic Setting

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
+import store, { history } from "./store";
+import MyRouter from "./routes/Router";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    // 모든 페이지에 store를 참조할 수 있도록 해줌
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <MyRouter />
+      </ConnectedRouter>
+    </Provider>
+  );
 };
 
 export default App;
---
+++ client/src/routes/Router.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const MyRouter = () => {
+  return (
+    <Fragment>
+      <Header />
+      <h1>Hello Body</h1>
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default MyRouter;
