b90dd45120de163c9e699e122c5a680622d42281
qnrjs42 <qnrjs42@naver.com>
2020-08-30 10:28:10 +0900

    Frontend - 22, Header 4 - LoginModal 1 // redux-saga 로그인 세팅

--- client/src/redux/sagas/index.js
+++ client/src/redux/sagas/index.js
@@ -1,7 +1,14 @@
-import { all } from "redux-saga/effects";
+import { all, fork } from "redux-saga/effects";
+import axios from "axios";
+
+import authSaga from "./authSaga";
+import dotenv from "dotenv";
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_BASIC_SERVER_URL;
 
 // function* 제너레이터 함수 : 여러 값을 반환
 
 export default function* rootSaga() {
-  yield all([]);
+  yield all([fork(authSaga)]);
 }
