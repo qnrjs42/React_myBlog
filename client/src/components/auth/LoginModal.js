8fa1f1da8714861734303c91f99790316b6fc24d
qnrjs42 <qnrjs42@naver.com>
2020-08-30 11:00:44 +0900

    Frontend - 23, Header 4 - LoginModal 2

--- client/src/components/auth/LoginModal.js
+++ client/src/components/auth/LoginModal.js
@@ -1,8 +1,103 @@
-import React from "react";
-import {} from "reactstrap";
+import React, { useState, useEffect } from "react";
+import {
+  NavLink,
+  Alert,
+  Button,
+  Label,
+  Input,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+} from "reactstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { CLEAR_ERROR_REQUEST, LOGIN_REQUEST } from "../../redux/types";
 
 const LoginModal = () => {
-  return <div>LoginModal</div>;
+  const [modal, setModal] = useState(false);
+  const [localMsg, setLocalMsg] = useState("");
+  const [form, setValues] = useState({
+    email: "",
+    password: "",
+  });
+  const dispatch = useDispatch();
+  const { errorMsg } = useSelector((state) => state.auth);
+
+  // []가 비워져있으면 렌더링 전에 한 번만 실행
+  // [errorMsg]가 있으면 errorMsg가 변화될 때마다 실행
+  useEffect(() => {
+    try {
+      setLocalMsg(errorMsg);
+    } catch (err) {
+      console.error(err);
+    }
+  }, [errorMsg]);
+
+  const handleToggle = () => {
+    dispatch({
+      type: CLEAR_ERROR_REQUEST,
+    });
+    setModal(!modal);
+  };
+
+  const onChange = (e) => {
+    setValues({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = form;
+    const user = { email, password };
+
+    console.log(user);
+
+    dispatch({
+      type: LOGIN_REQUEST,
+      payload: user,
+    });
+  };
+
+  return (
+    <div>
+      <NavLink onClick={handleToggle} href="#">
+        Login
+      </NavLink>
+      <Modal isOpen={modal} toggle={handleToggle}>
+        <ModalHeader toggle={handleToggle}>Login</ModalHeader>
+        <ModalBody>
+          {localMsg ? <Alert color="danger">{localMsg}</Alert> : null}
+          <Form onSubmit={onSubmit}>
+            <FormGroup>
+              <Label form="email">Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email"
+                onChange={onChange}
+              />
+
+              <Label form="password">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+                onChange={onChange}
+              />
+              <Button color="dark" style={{ marginTop: "2rem" }} block>
+                Login
+              </Button>
+            </FormGroup>
+          </Form>
+        </ModalBody>
+      </Modal>
+    </div>
+  );
 };
 
 export default LoginModal;
