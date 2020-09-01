677617ae72605c781058ff471572d0305383fdbf
qnrjs42 <qnrjs42@naver.com>
2020-09-01 13:08:40 +0900

    Frontend - 38 CKEditor For React

--- client/src/routes/normalRoute/PostWrite.js
+++ client/src/routes/normalRoute/PostWrite.js
@@ -1,9 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  Form,
+  FormGroup,
+  Button,
+  Col,
+  Progress,
+  Label,
+  Input,
+} from "reactstrap";
+import CKEditor from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
+
+import { editorConfiguration } from "../../components/editor/EditorConfig";
+import MyInit from "../../components/editor/UploadAdapter";
 
 const PostWrite = () => {
+  const { isAuthenticated } = useSelector((state) => state.auth);
+  const [form, setValues] = useState({ title: "", contents: "", fileUrl: "" });
+  const dispatch = useDispatch();
+
+  const onChange = (e) => {
+    setValues({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = async (e) => {
+    await e.preventDefault();
+
+    const { title, contents, fileUrl, category } = form;
+  };
+
+  const getDataFromCKEditor = (event, editor) => {
+    console.log("editor");
+  };
+
   return (
     <>
-      <h1>PostWrite</h1>
+      {isAuthenticated ? (
+        <Form>
+          <FormGroup className="mb-3">
+            <Label for="title">Title</Label>
+            <Input
+              type="text"
+              name="title"
+              id="title"
+              className="form-control"
+              onChange={onChange}
+            />
+          </FormGroup>
+          <FormGroup className="mb-3">
+            <Label for="category">Category</Label>
+            <Input
+              type="text"
+              name="category"
+              id="category"
+              className="form-control"
+              onChange={onChange}
+            />
+          </FormGroup>
+          <FormGroup className="mb-3">
+            <Label for="content">Content</Label>
+            {/* onChange가 아니라 onBlur인 이유는
+              onChange는 한 문자 바뀔 때마다 함수 호출하고 리렌더링되어 성능 저하될 염려가 있기 때문
+            */}
+            <CKEditor
+              editor={ClassicEditor}
+              config={editorConfiguration}
+              onInit={MyInit}
+              onBlur={getDataFromCKEditor}
+            />
+            <Button
+              color="success"
+              block
+              className="mt-3 col-md-2 offset-md-10 mb-3"
+            >
+              제출하기
+            </Button>
+          </FormGroup>
+        </Form>
+      ) : (
+        <Col width={50} className="p-5 m-5">
+          <Progress animated color="info" value={100} />
+        </Col>
+      )}
     </>
   );
 };
