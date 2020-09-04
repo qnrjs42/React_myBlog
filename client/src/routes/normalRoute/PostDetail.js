95a7d4de948f78bdd07f668b9efe8699a58c421d
qnrjs42 <qnrjs42@naver.com>
2020-09-04 19:24:59 +0900

    Frontend - 44, Post Detail 3

--- client/src/routes/normalRoute/PostDetail.js
+++ client/src/routes/normalRoute/PostDetail.js
@@ -1,35 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {} from "react-helmet";
+import { Helmet } from "react-helmet";
 import {
   POST_DETAIL_LOADING_REQUEST,
   POST_DELETE_REQUEST,
   USER_LOADING_REQUEST,
 } from "../../redux/types";
-import { Row, Col, Button } from "reactstrap";
+import { Button, Row, Col, Container } from "reactstrap";
 import { Link } from "react-router-dom";
 import CKEditor from "@ckeditor/ckeditor5-react";
+import { GrowingSpinner } from "../../components/spinner/Spinner";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPencilAlt,
+  faCommentDots,
+  faMouse,
+} from "@fortawesome/free-solid-svg-icons";
+import BalloonEditor from "@ckeditor/ckeditor5-editor-balloon/src/ballooneditor";
+import { editorConfiguration } from "../../components/editor/EditorConfig";
+// import Comments from "../../components/comments/Comments";
 
 const PostDetail = (req) => {
   const dispatch = useDispatch();
-  const { PostDetail, creatorId, title, loading } = useSelector(
+  const { postDetail, creatorId, title, loading } = useSelector(
     (state) => state.post
   );
   const { userId, userName } = useSelector((state) => state.auth);
+  // const { comments } = useSelector((state) => state.comment);
 
   console.log(req);
-
   useEffect(() => {
     dispatch({
       type: POST_DETAIL_LOADING_REQUEST,
       payload: req.match.params.id,
     });
-
     dispatch({
       type: USER_LOADING_REQUEST,
       payload: localStorage.getItem("token"),
     });
-  });
+  }, [dispatch, req.match.params.id]);
 
   const onDeleteClick = () => {
     dispatch({
@@ -42,14 +51,13 @@ const PostDetail = (req) => {
   };
 
   const EditButton = (
-    <>
+    <Fragment>
       <Row className="d-flex justify-content-center pb-3">
         <Col className="col-md-3 mr-md-3">
           <Link to="/" className="btn btn-primary btn-block">
             Home
           </Link>
         </Col>
-
         <Col className="col-md-3 mr-md-3">
           <Link
             to={`/post/${req.match.params.id}/edit`}
@@ -58,18 +66,17 @@ const PostDetail = (req) => {
             Edit Post
           </Link>
         </Col>
-
         <Col className="col-md-3">
           <Button className="btn-block btn-danger" onClick={onDeleteClick}>
             Delete
           </Button>
         </Col>
       </Row>
-    </>
+    </Fragment>
   );
 
   const HomeButton = (
-    <>
+    <Fragment>
       <Row className="d-flex justify-content-center pb-3">
         <Col className="col-sm-12 com-md-3">
           <Link to="/" className="btn btn-primary btn-block">
@@ -77,15 +84,38 @@ const PostDetail = (req) => {
           </Link>
         </Col>
       </Row>
-    </>
+    </Fragment>
   );
 
-  console.log(title);
-
-  return (
+  const Body = (
     <>
-      <h1>PostDetail</h1>
+      {userId === creatorId ? EditButton : HomeButton}
+      <Row className="border-bottom border-top border-primary p-3 mb-3 justify-content-between">
+        {(() => {
+          if (postDetail && postDetail.creator) {
+            return (
+              <>
+                <div className="font-weight-bold text-big">
+                  <span className="mr-3">
+                    <Button color="info">
+                      {postDetail.category.categoryName}
+                    </Button>
+                  </span>
+                  {postDetail.title}
+                </div>
+                <div className="align-self-end">{postDetail.creator.name}</div>
+              </>
+            );
+          }
+        })()}
+      </Row>
     </>
+  );
+  return (
+    <div>
+      <Helmet title={`Post | ${title}`} />
+      {loading === true ? GrowingSpinner : Body}
+    </div>
   );
 };
 
